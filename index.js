332be6e37f05e124f34b055552efeccc6d9078d7
kokopuffz <chanheeya@gmail.com>
2022-02-02 22:46:57 -0600

    fill screen with pixels

--- index.js
+++ index.js
@@ -2,18 +2,35 @@
 console.log('I can do this! maybe *\o/')
 
 const pixel = document.querySelector('.pixel')
-const screen = document.querySelector('.screen')
+const screen = document.querySelector('#screen')
 
-const change = (event) => {
-  event.target.classList.add('colored-in')
+const mouseEnter = (event) => {
+  pixel.classList.add('.colored-in');
+  console.log(event)
+
 }
 
+// event.onmouseenter = change;
 
-pixel.onmouseenter = change;
 
-// function mouseEnter() {
-//   pixels.classList.add(.colored-in);
+const makeScreen = () => {
+  for (let i = 0; i < 2800; i++) {
+    const square = document.createElement('pixel')
+    square.classList.add('pixel')
+    screen.appendChild(square)
+  }
+
+}
+
+// const change = (event) => {
+//   event.target.classList.add('colored-in')
+  
 // }
+// 
+
+makeScreen()
+
+
 // //add pixel divs to the dom
 // // document.addEventListener('DOMContentLoaded', ()=> {
 // //   // console.log()
