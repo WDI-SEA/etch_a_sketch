3f8c345941b8a56dc59e1f01b3d709def5d80486
romanprotoliuk <romanprotoliuk@gmail.com>
2022-02-03 10:42:56 -0800

    Etch

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // The magic happens here!
+// const showBlackSquare = () => {
+//   const pixel = document.querySelector(".pixel");
+//   pixel.addEventListener("mouseenter", () => {
+//     pixel.classList.add("colored-in");
+//   });
+// };
+
+const resetBtn = () => {
+  const pixels = document.querySelectorAll(".pixel");
+  for (let i = 0; i < pixels.length; i++) {
+    pixels[i].classList.remove("colored-in");
+  }
+};
+
+const generatePixels = () => {
+  // need to get height of the whole
+  for (i = 1; i < 10000; i++) {
+    let singleDiv = document.createElement("div");
+
+    singleDiv.classList.add("pixel");
+
+    document.getElementById("screen").appendChild(singleDiv);
+    singleDiv.addEventListener("mouseenter", () => {
+      singleDiv.classList.add("colored-in");
+    });
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  generatePixels();
+  document
+    .getElementById("clear-screen-button")
+    .addEventListener("click", resetBtn);
+});
+``;
