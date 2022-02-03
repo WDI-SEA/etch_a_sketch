ac4740e8b190d37083bfa2bd28cce1a8cfa4bc81
mvanzo <martin.vanzo@gmail.com>
2022-02-02 20:52:41 -0800

    etch a sketch deliverable

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// The magic happens here!
+
+// Pixel one colored black during mouse enter event
+const etch = () => {
+    const pix = document.querySelector('.pixel')
+    pix.addEventListener('mouseenter', () => {
+        pix.classList.add('colored-in')
+    })
+}
+
+// fill etch-a-sketch with pixels
+const fillBoard = (numPixels) => {
+        for (let i = 0; i<numPixels; i++) {
+        //create div elements to fill board
+        const newPix = document.createElement('div');
+        // append child to screen id
+        document.querySelector('#screen').appendChild(newPix);
+        // add styling
+        newPix.classList.add('pixel');
+        //etch all
+        newPix.addEventListener('mouseenter', () => {
+            newPix.classList.add('colored-in')
+    })
+    }
+}
+
+// clear screen
+let clearBoard = () => {
+    const allPix = document.querySelectorAll('.pixel');
+    const clearButton = document.getElementById('clear-screen-button');
+    clearButton.addEventListener('click', () => {
+        for (let i=0; i<allPix.length; i++) {
+            allPix[i].classList.remove('colored-in');
+    }
+})
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    etch();
+    fillBoard(5600);
+    clearBoard();
+})
