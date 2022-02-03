3065af961fb71ff919f3615038fefbe774d6bf9a
Sol Youn <syoun93@gmail.com>
2022-02-02 19:53:59 -0800

    updated js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// The magic happens here!
+const screen = document.querySelector("#screen")
+const clearButton = document.querySelector("#clear-screen-button")
+
+const createPixels = (numOfPixels) => {
+  for (let i = 0; i < numOfPixels; i++) {
+    const pixels = document.createElement("div")
+    pixels.classList.add("pixel")
+    screen.appendChild(pixels)
+    pixels.addEventListener("mouseenter", fillPixels)
+  }
+}
+
+const fillPixels = (event) => {
+  event.target.classList.add("colored-in")
+}
+
+const clearScreen = () => {
+  document.querySelectorAll(".pixel").forEach((element) => {
+    element.classList.remove("colored-in")
+  })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  createPixels(6000)
+  clearButton.addEventListener("click", clearScreen)
+})
