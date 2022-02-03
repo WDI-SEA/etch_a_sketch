a76aa62eaf17119018073ae89fe722c485c1a2a8
br1anlee <joonmolee3@gmail.com>
2022-02-02 22:28:39 -0800

    etch_a_sketch_solution

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// The magic happens here!
+//DOM Elements
+const clearButton = document.querySelector('#clear-screen-button')
+// const allPixel = document.querySelectorAll(".pixel")
+
+const createPixel = () => {
+    const screen = document.querySelector('#screen')
+    for (let i = 0; i < 5081; i++) {
+        //create a new div
+        let smallPixel = document.createElement('div')
+        smallPixel.classList.add("pixel")
+        screen.appendChild(smallPixel)
+        
+
+        smallPixel.addEventListener('mouseenter', pixelColor)
+    }
+}
+
+const pixelColor = (event) => {
+    event.target.classList.add('colored-in')
+}
+
+const clearBtn = () => {
+    const allPixel = document.querySelectorAll(".pixel")
+    for (let i = 0; i < 5081; i++) {
+        allPixel[i].classList.remove('colored-in')
+    }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    createPixel()
+    clearScreen = document.addEventListener('click', clearBtn)
+})
