89ebf1853005b2f975472de3fbca7e2af9802c1d
Professorsurf <guevara.a.gabriel@gmail.com>
2022-02-03 10:50:13 -0800

    deliverable 2-2

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // The magic happens here!
+document.addEventListener('DOMcontentLoadad', ()=>{
+
+const etch = () => {
+    const pix = document.querySelector('.pixel')
+    pix.addEventListener('mouseenter', ()=>{
+    pix.classList.add('colored-in')
+})
+}
+
+const generatePixels = () => {
+    for(let i=0; i<50; i++) {
+        let singleDiv = document.createElement('div')
+        singleDiv.classList.add('pixels')
+        document.getElementById('screen').appendChild(singleDiv)
+        singleDiv.addEventListener('mouseenter', ()=>{
+            singleDiv.classList.add('colored-in')
+        })
+    }
+}
+
+
+const clearScreen = () => {
+    // remove 'colored in ' class from all pixels
+    const pixels = document.querySelectorAll('pixel')
+    for(let i=0; i<pixels.length; i++) {
+        pixels[i].classList.remove('coloredo-in')
+    }
+}
+
+
+    const clearBtn = document.getElementById('clear-screen-button')
+    generatePixels()
+    document.getElementById.apply('clear-screen-button').addEventListener('click',clearScreen)
+})
