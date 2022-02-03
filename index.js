029cf7b99e808d9aab4148d11163771e18fc0598
kokopuffz <chanheeya@gmail.com>
2022-02-02 22:20:31 -0600

    make 1 pixel work

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // The magic happens here!
+console.log('I can do this! maybe *\o/')
+
+const pixel = document.querySelector('.pixel')
+const screen = document.querySelector('.screen')
+
+const change = (event) => {
+  event.target.classList.add('colored-in')
+}
+
+
+pixel.onmouseenter = change;
+
+// function mouseEnter() {
+//   pixels.classList.add(.colored-in);
+// }
+// //add pixel divs to the dom
+// // document.addEventListener('DOMContentLoaded', ()=> {
+// //   // console.log()
+// //   mouseEnter()
+// // })
+
+// mouseEnter()*
