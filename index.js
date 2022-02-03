0174a6b41aa5d8e9eb21e3b6361ea3c1c76f246f
danieldenton <danielmdenton@gmail.com>
2022-02-03 09:32:56 -0800

    etchasketch

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// The magic happens here!
+
+const screen = document.querySelector('#screen')
+const pixel = document.querySelectorAll('.pixel')
+const clearButton = document.getElementById('clear-screen-button')
+
+for (let i = 0; i <4525; i++) {
+        let div = document.createElement('div')
+        div.classList.add('pixel')
+        screen.appendChild(div)
+    }
+    document.querySelectorAll('.pixel').forEach(p =>
+    p.addEventListener('mouseover', e => {
+      e.target.classList.toggle('colored-in');
+    }),
+  );
+
+  function clear() {
+    document.querySelectorAll('.pixel').forEach(p => p.classList.remove('colored-in'))
+  }
+ 
+  clearButton.addEventListener('click', clear)
+
+  
