09fa82a1247bb149f886956cc22110d96a3837f7
justin-aquino <justin.aquino25@gmail.com>
2022-02-02 19:49:40 -0800

    submitting etch a sketch homework

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // The magic happens here!
+// const pixel1 = document.querySelector(".pixel")
+
+
+// pixel1.addEventListener("mouseenter", () => {
+//     pixel1.style.backgroundColor = "black"
+// })
+
+const clearBtn = document.getElementById("clear-screen-button")
+
+const boardGenerator = (numOfPixels) => {
+    for ( let x = 1; x < numOfPixels; x++){
+        const pixels = document.createElement("div")
+        pixels.classList.add("pixel")
+        document.getElementById("screen").appendChild(pixels)
+        pixels.addEventListener("mouseenter", () => {
+            pixels.classList.add("colored-in")
+        clearBtn.addEventListener("click", () => {
+                // const pixels = document.querySelectorAll(".pixel")
+            pixels.classList.remove("colored-in")
+            })
+        })
+    }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    boardGenerator(6175)
+})
