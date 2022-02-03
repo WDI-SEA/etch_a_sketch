6b55a48a64d45654635042e137c8f19e809ae7d9
Norman Teodoro <norman@FooSasugaome-LegionT730.localdomain>
2022-02-02 21:22:47 -0800

    code cleanup

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
 // // The magic happens here!
-const sketchArea = document.querySelector("screen");
+const buttonClear = document.getElementById("clear-screen-button");
 
 const createPixels = (pixelCount) => {
   for (let i = 1; i <= pixelCount; i++) {
     const pixel = document.createElement("div");
-    pixel.setAttribute("id", i);
     pixel.classList.add("pixel");
     document.getElementById("screen").appendChild(pixel);
+    pixel.addEventListener("mouseenter", () => {
+      pixel.classList.add("colored-in");
+    });
+    buttonClear.addEventListener("click", () => {
+      pixel.classList.remove("colored-in")
+    })
+  
   }
+  
 };
 
 const etchArea = () => {
   const height = document.getElementById("screen").offsetHeight;
   const width = document.getElementById("screen").offsetWidth;
-//   console.log(`${width} X ${height}`);
+  //   console.log(`${width} X ${height}`);
   //const area = (width *height)/100;
   const area = 6400;
   return area;
 };
 
-//fills etch area
-createPixels(etchArea());
-
-const etch = (event) => {
-  if (event.target.nodeName === "DIV") {
-    document.getElementById(event.target.id).classList.add("colored-in");
-  }
-};
-
-const clearSketchArea = (event) => {
-  if (event.target.nodeName === "BUTTON") {
-    for (let i = 1; i <= etchArea(); i++) {
-      document.getElementById(i).classList.remove("colored-in");
-    }
-  }
-};
-
-window.addEventListener("mouseover", etch);
-window.addEventListener("click", clearSketchArea);
+document.addEventListener("DOMContentLoaded", () => {
+  createPixels(etchArea());
+});
