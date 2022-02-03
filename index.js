fed4df2fbe65d5ea9f97318037b875cfeea8ba2c
kokopuffz <chanheeya@gmail.com>
2022-02-03 00:58:32 -0600

    make all pixels work like original

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 // The magic happens here!
 console.log('I can do this! maybe *\o/')
 
-const pixel = document.querySelector('.pixel')
-const screen = document.querySelector('#screen')
-
-const mouseEnter = (event) => {
-  pixel.classList.add('.colored-in');
-  console.log(event)
-
-}
-
+// const mouseenter = (event) => {
+//   let pixels = document.querySelectorAll('.pixel')
+//   // pixels.classList.add('.colored-in') //currently a nodelist
+//   // event.preventDefault();
+//   console.log(pixels)
+// }
 // event.onmouseenter = change;
+// const screen = document.querySelector('#screen')
+// const pixel = document.getElementsByTagName('.pixel')
+// const change = ()=> {
 
+// } document.getElementsByName('colored-in')
 
 const makeScreen = () => {
   for (let i = 0; i < 2800; i++) {
-    const square = document.createElement('pixel')
+    const square = document.createElement('div')
     square.classList.add('pixel')
-    screen.appendChild(square)
+    document.querySelector('#screen').appendChild(square)
+    console.log(square.length)
+
+    const change = (event) => {
+      event.target.classList.add('colored-in')
+    }
+
+    square.onmouseenter= change
+    // square.addEventListener('event', () => {
+      
+      
+    // })
+    // let pixelss = document.querySelectorAll('pixel')
+    // const change = (event) => {
+    // console.log(typeof pixelss)
   }
 
 }
@@ -26,10 +41,21 @@ const makeScreen = () => {
 //   event.target.classList.add('colored-in')
   
 // }
-// 
 
-makeScreen()
 
+document.addEventListener('DOMContentLoaded', () => {
+  makeScreen()
+  // mouseenter()
+//   // const mouseEnter = (event) => {
+//   //   // pixel.pixel.classList.add('.colored-in');
+//   //   // event.preventDefault();
+//   //   console.log(event)
+//   // }
+})
+
+// makeScreen()
+// mouseEnter()
+// console.log(pixel)
 
 // //add pixel divs to the dom
 // // document.addEventListener('DOMContentLoaded', ()=> {
