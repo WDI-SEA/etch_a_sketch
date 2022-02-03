1be019449e2bbbfccb405ae3cd0093ce459f5c0a
kokopuffz <chanheeya@gmail.com>
2022-02-03 01:32:59 -0600

    clear screen button working at 75%

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 // The magic happens here!
 console.log('I can do this! maybe *\o/')
 
-// const mouseenter = (event) => {
-//   let pixels = document.querySelectorAll('.pixel')
-//   // pixels.classList.add('.colored-in') //currently a nodelist
-//   // event.preventDefault();
-//   console.log(pixels)
-// }
-// event.onmouseenter = change;
-// const screen = document.querySelector('#screen')
-// const pixel = document.getElementsByTagName('.pixel')
-// const change = ()=> {
-
-// } document.getElementsByName('colored-in')
 
 const makeScreen = () => {
   for (let i = 0; i < 2800; i++) {
@@ -24,43 +12,22 @@ const makeScreen = () => {
     const change = (event) => {
       event.target.classList.add('colored-in')
     }
-
     square.onmouseenter= change
-    // square.addEventListener('event', () => {
-      
-      
-    // })
-    // let pixelss = document.querySelectorAll('pixel')
-    // const change = (event) => {
-    // console.log(typeof pixelss)
+
+    document.addEventListener('click', event => {
+      square.classList.remove("pixel")
+      console.log(square)
+    })
+    
   }
-
-}
-
-// const change = (event) => {
-//   event.target.classList.add('colored-in')
   
-// }
-
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   makeScreen()
-  // mouseenter()
-//   // const mouseEnter = (event) => {
-//   //   // pixel.pixel.classList.add('.colored-in');
-//   //   // event.preventDefault();
-//   //   console.log(event)
-//   // }
 })
 
-// makeScreen()
-// mouseEnter()
-// console.log(pixel)
+document.addEventListener('click', () => {
+  makeScreen()
+})
 
-// //add pixel divs to the dom
-// // document.addEventListener('DOMContentLoaded', ()=> {
-// //   // console.log()
-// //   mouseEnter()
-// // })
-
-// mouseEnter()*
