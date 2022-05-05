219a1b717d53f29372ade29a51391509eb28a6b3
Billy Lu <billyfwlu@gmail.com>
2022-05-04 17:39:48 -0700

    initial commit, finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // The magic happens here!
+const clearBtn = document.querySelector("#clear-screen-button")
+const screen = document.querySelector("#screen")
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("hiii")
+    const firstpixel = document.querySelector(".pixel")
+    firstpixel.addEventListener("mouseenter",boxFill)
+
+    for(i=0; i<8099;i++){
+        const box = document.createElement("div")
+        box.classList.add("pixel")
+        box.addEventListener("mouseenter",boxFill)
+        screen.append(box)
+    }
+    clearBtn.addEventListener("click",clearAll)
+})
+
+function boxFill(event) {
+    event.target.classList.add("colored-in")
+}
+
+function clearAll() {
+    const pixels = document.querySelectorAll(".pixel")
+    pixels.forEach(function(j){
+        j.classList.remove("colored-in")
+    })
+
+
+}
