31885f90790f932b24b40d59d4c44303baa8d709
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-05-04 22:57:17 -0500

    fills out grid with pixels

--- index.js
+++ index.js
@@ -10,14 +10,28 @@
 
 const pixel = document.querySelector('.pixel')
 
+const screen = document.getElementById('screen')
+const pixelBoard = () => {
+    //multiply the number of pixel divs
+    for(let i = 0; i < 7496; i++){
+        // create new div
+        const add = document.createElement('div')
+        // add css class for pixel shape
+        add.classList.add('pixel')
+        // append div to the container
+        document.querySelector('#screen').append(add)
+    } 
+}
+
 document.addEventListener('DOMContentLoaded', ()=>{
     
     pixel.addEventListener('mouseenter', event => {
         pixel.style.backgroundColor = 'black'
         console.log('mouseenter')
     })
+    pixelBoard()
     
-  
+    
 
 
 
