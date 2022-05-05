d219ac4e85c2bb7360d12ef3427e29c65dcf859e
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-05-04 22:14:35 -0500

    adds mouseenter event

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // The magic happens here!
+
+// try to get mouseenter to work
+// listen to the mouse enter event
+// target is the .pixel class
+// apply the #colored-in class to the div
+// no mouseleave event
+
+// clear button will return all divs to #pixel class
+
+const pixel = document.querySelector('.pixel')
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    
+    pixel.addEventListener('mouseenter', event => {
+        pixel.style.backgroundColor = 'black'
+        console.log('mouseenter')
+    })
+    
+  
+
+
+
+
+
+
+})
