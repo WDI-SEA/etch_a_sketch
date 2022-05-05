df3bebc0515d7749ccab4b0597456f100ee3c928
DarcXnite <anhri89@yahoo.com>
2022-05-04 19:12:37 -0500

    davis finished etch

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// The magic happens here!
+// pixels.addEventListener("mouseenter", () => {
+//   pixels.classList.add("colored-in");
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const screen = document.querySelector("#screen");
+
+  for (let i = 0; i <= 6467; i++) {
+    const pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+
+    pixel.addEventListener("mouseenter", () => {
+      pixel.classList.add("colored-in");
+    });
+
+    screen.appendChild(pixel);
+  }
+});
+
+document.querySelector("#clear-screen-button").addEventListener("click", () => {
+  const pixels = document.querySelectorAll(".pixel");
+
+  for (let i = 0; i < pixels.length; i++) {
+    pixels[i].classList.remove("colored-in");
+  }
+});
