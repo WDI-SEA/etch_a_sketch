09c1706f4f6f0b4f171719a469939df187e36790
ApexBortz <nicholas@georgetownframeshoppe.com>
2022-05-04 20:38:39 -0700

    N-Bortz makes an Etch-a-sketch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// The magic happens here!
+const clearButton = document.getElementById('clear-screen-button')
+const screen = document.getElementById('screen')
+
+document.addEventListener('DOMContentLoaded', function(){
+    // console.log('oh hi there, i work')
+    const firstPixel = document.querySelector('.pixel')
+    firstPixel.addEventListener('mouseenter', boxFill)
+
+    for (i=0; i<8500; i++){
+        const screenBox = document.createElement('div')
+        screenBox.classList.add('pixel')
+        screenBox.addEventListener('mouseenter', boxFill)
+        screen.append(screenBox)
+    }
+    clearButton.addEventListener('click', clearAll)
+})
+
+function boxFill(event) {
+    event.target.classList.add('colored-in')
+}
+
+function clearAll() {
+    const pixels = document.querySelectorAll('.pixel')
+    pixels.forEach(function(e){
+        e.classList.remove('colored-in')
+    })
+}
