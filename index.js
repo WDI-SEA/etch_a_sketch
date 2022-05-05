834f10344346348ae05f966e6701c8a8a470147c
seiplet93 <seiplet93@gmail.com>
2022-05-05 13:10:15 -0400

    deliverable, etch a sketch

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// The magic happens here!
+const screen = document.getElementById("screen");
+const pixel = document.querySelector(".pixel");
+
+document.addEventListener("mouseover", (e) => {
+  if (e.target.className === "pixel") {
+    console.log("hello?");
+    let ppixel = e.target;
+    ppixel.classList.add("colored-in");
+  }
+});
+
+const addPix = function () {
+  for (i = 0; i < 6000; i++) {
+    const newpix = document.createElement("div");
+    newpix.classList.add("pixel");
+    screen.append(newpix);
+  }
+};
+addPix();
+
+const btn = document.getElementById("clear-screen-button");
+btn.addEventListener("click", function () {
+  function clearAll() {
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(function (j) {
+      j.classList.remove("colored-in");
+    });
+    clearAll();
+  }
+
+  //   let coloredIn = document.querySelectorAll("div");
+
+  //   //   console.log("yo??????");
+  //   coloredIn.classList.remove("colored-in");
+
+  //   //   document.querySelectorALL("colored-in").classList.remove("colored-in");
+
+  //   console.log("yo");
+});
+// document.querySelectorAll("div").classList.remove("colored-in");
