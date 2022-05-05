7faf1704566b1f72b44ed418498fd8a09d23918d
marzo20 <myjune91@gmail.com>
2022-05-04 22:06:53 -0700

    sketch done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// The magic happens here!
+
+const clear = document.querySelector('#clear-screen-button')
+document.addEventListener('DOMContentLoaded', () => {
+    for( let i=0; i<4000; i++){
+        const pixel = document.createElement('div')
+        pixel.classList.add('pixel')
+        document.querySelector('#screen').appendChild(pixel)
+        pixel.addEventListener('mouseenter', () => {
+        pixel.classList.add('colored-in')
+        
+        })
+
+}
+clear.addEventListener('click', () => {
+    const pixels = document.querySelectorAll('.pixel')
+    for(let i = 0; i < pixels.length; i++){
+    
+    pixels[i].classList.remove('colored-in')
+    }
+})     
+    
+})
+   
