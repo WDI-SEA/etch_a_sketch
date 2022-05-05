16c383fe70629a6b01006943bf38a8f2baf0273f
Eduarte783 <eric.duarte83@gmail.com>
2022-05-05 09:00:39 -0700

    etch a sketch deliverable

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// The magic happens here!
+const screen = document.getElementById("screen")
+
+for(let i = 0; i < 6423; i++) {
+    let pixel = document.createElement("div")
+    pixel.classList.add("pixel")
+  
+    pixel.addEventListener("mouseenter", (e) => {
+    e.target.classList.add("colored-in")
+    })
+    screen.append(pixel)
+}
+const clearButton = document.getElementById("clear-screen-button")
+
+
+clearButton.addEventListener("click", () => {
+    const pixelList = document.querySelectorAll(".pixel")
+    console.log(pixelList)
+    pixelList.forEach(element=>element.classList.remove("colored-in"))
+})
