1df210a8fa2671988e9ceaa5d191531c05ce843f
“j-onederfulgit config --global user.email “jackwwyman@gmail.com <jackwyman@macbook-pro-2.lan>
2022-05-04 20:01:06 -0700

    done did it

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// The magic happens here!
+const screen = document.querySelector('#screen')
+const clearButton = document.querySelector('#clear-screen-button')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const pixel = document.querySelector('.pixel')
+    pixel.addEventListener('mouseenter', darken)
+    
+    for (let i = 0; i < 4223; i++) {
+        const newPixel = document.createElement('div')
+        newPixel.classList.add('pixel')
+        newPixel.addEventListener('mouseenter', darken)
+        screen.append(newPixel)
+    }
+    clearButton.addEventListener('click', clearAll)
+})
+
+const clearAll = () => {
+    const pixels = document.querySelectorAll('.pixel')
+    pixels.forEach(function(j) {
+        j.classList.remove('colored-in')
+    })
+}
+
+
+const darken = e => {
+    e.target.classList.add('colored-in')
+}
