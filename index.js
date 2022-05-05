a9484fe2ed62e16911b501acb60b18968635fb81
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-05-04 18:02:31 -0600

    Create etch-a-sketch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// The magic happens here!
+const markPixel = (e) => {
+  const pixel = e.currentTarget
+  pixel.classList.add("colored-in")
+}
+
+const generatePixels = () => {
+  const screen = document.querySelector("#screen")
+
+  for (let i = 0; i < 5000; i++) {
+    const pixel = document.createElement("div")
+    pixel.classList.add("pixel")
+    screen.appendChild(pixel)
+
+    pixel.addEventListener("mouseenter", markPixel)
+  }
+}
+
+const clearScreen = () => {
+  const pixels = [...document.querySelectorAll(".pixel")]
+  pixels.forEach((pixel) => pixel.classList.remove("colored-in"))
+}
+
+generatePixels()
+
+document
+  .querySelector("#clear-screen-button")
+  .addEventListener("click", clearScreen)
