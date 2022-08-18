0e3f469c91125848c32be0010435abb37373cc83
aqsakhatoon <khanabrar_02@yahoo.com>
2022-08-17 23:41:15 -0500

    aqsa etch a sketch

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// The magic happens here!
+document.addEventListener("DOMContentLoaded", () => {
+    let screen = document.querySelector("#screen")
+    let clearButton = document.querySelector("#clear-screen-button")
+
+    let mouseHover = (e) => {
+        e.target.classList.add("colored-in")
+    }  
+
+    let etchBoard = () => {
+        for (i = 0; i < 8000; i++) {
+            const board = document.createElement("div")
+            board.classList.add("pixel")
+            board.addEventListener("mouseover", mouseHover)
+            screen.append(board)
+           }
+    }
+    etchBoard()
+
+    clearButton.addEventListener("click", () => {
+        while(screen.firstChild) {
+            screen.removeChild(screen.firstChild)
+        }
+        for (i = 0; i < 8000; i++) {
+            const board = document.createElement("div")
+            board.classList.add("pixel")
+            board.addEventListener("mouseover", mouseHover)
+            screen.append(board)
+    }
+})
+})
