8f234b3e452d9bd68203c744cc92c15d56017b0f
elek8871 <elekmichelle@gmail.com>
2022-08-17 21:02:48 -0600

    adds color changing

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// The magic happens here!
+
+document.addEventListener('DOMContentLoaded', () => {
+
+ const screen = document.getElementById("screen")
+ const clearButton= document.getElementById("clear-screen-button")
+
+
+
+ for (let i=0; i <10000; i++) {
+    const addPixel = document.createElement("div")
+    addPixel.classList.add("pixel") 
+    screen.append(addPixel)
+}
+const pixels = document.querySelectorAll(".pixel")
+pixels.forEach(pixel => {
+    pixel.addEventListener("mouseenter", (e) =>{
+        pixel.style.backgroundColor = "black"
+    
+    } )
+    
+})
+
+clear.addEventListener('click', (e) => {
+    // clear out both list while loops
+    pixels.forEach(pixel => {
+    pixel.style.backgroundColor = 'white'
+})
+})
+
+// const clearScreen = document.querySelectorAll(".clear-screen-button")
+// clearButton.addEventListener
+// pixels.forEach(pixel => {
+//     pixel.style.backgroundColor ="white"
+})
+
+
