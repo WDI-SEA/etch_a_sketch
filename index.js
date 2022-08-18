dfbbc739bae3074613fc8d470dfc37ba71d2ccec
brandon-w1205 <bwu1205@gmail.com>
2022-08-17 19:46:11 -0700

    adds etchasketch() function and gives up on adding click functionality to board

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // The magic happens here!
 document.addEventListener('DOMContentLoaded', () => {
     
-
     let etchBox = document.querySelector('#screen');
     let clear = document.querySelector('#clear-screen-button');
     
@@ -10,40 +9,23 @@ document.addEventListener('DOMContentLoaded', () => {
         makePixels.setAttribute("class", "pixel")
         etchBox.appendChild(makePixels);
     }
+
     let p = document.querySelectorAll('.pixel');
-
-    let onOff = 0;
-    function etchasketch() {
-        onOff++
-        console.log(onOff)
-        if(onOff % 2 != 0){
-            p.forEach(e => {
-                e.addEventListener('mouseenter', () => {
-                    e.style.backgroundColor = 'rgb(0, 0, 0)';
-                })
-            })
-        } else {
-            p.forEach(e => {
-                e.removeEventListener('mouseenter', () => {
-                    e.style.backgroundColor = 'rgb(0, 0, 0)';
-                })
-            })
-        }
-        
-    }
-
     
+    function etchasketch() {
+        p.forEach(e => {
+            let test = function() {
+                e.style.backgroundColor = 'rgb(0, 0, 0)';
+            }
+            e.addEventListener('mouseenter', test)
+        })
+    }  
 
-    etchBox.addEventListener('click', etchasketch)
+    etchasketch()
     
     clear.addEventListener('click', () => {
         p.forEach(e => {
-            e.style.backgroundColor = 'rgb(255, 255, 255';
+            e.style.backgroundColor = 'rgb(255, 255, 255)';
         })
-    })
-    
-        
-
-    
-    
+    })   
 })
