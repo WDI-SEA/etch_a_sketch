5730bfa92d69664a6412178bf522b34e8f59ea83
waltersmatthew <waltersmatthew88@gmail.com>
2022-08-17 18:47:03 -0700

    finish deliverable

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// The magic happens here!
+document.addEventListener("DOMContentLoaded", () =>{
+//variables
+    const screen = document.querySelector('#screen')
+    const pixel = document.querySelector('.pixel')
+
+//add all the pixels 
+    for(let i = 0; i < 6000; i++){
+        let newPixel = document.createElement('div')
+        newPixel.classList.add('pixel')
+        screen.appendChild(newPixel)    
+    }  
+
+ //grab all the pixels
+    const allPixels = document.querySelectorAll('.pixel')
+        // console.log(allPixels)
+    //loop through and add all the classes
+        allPixels.forEach(pixel => {
+            pixel.addEventListener('mouseenter', (e) => {
+                pixel.classList.add("colored-in")
+    })})
+
+//clear button function  
+    document.querySelector('#clear-screen-button').addEventListener("click", (e) => {
+        allPixels.forEach(pixel =>{
+            pixel.classList.remove("colored-in")
+        })
+    })
+//nothing below here    
+})
+ 
