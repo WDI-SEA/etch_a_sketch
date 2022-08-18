cdd92e30ee275a8365fd28ca42579ed520fbfe8e
Moodybleu <meganbenn27@icloud.com>
2022-08-18 07:36:54 -0700

    starting to fill in the proper values and parameters

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-const makePalette = () => {
-    const colorPalette = document.querySelector('#color-palette')
+const sketch = () => {
+    const sketch = document.querySelector('#screen')
 
     // while something is in the element, remove it
-    while (colorPalette.firstChild) {
+    while (sketch.firstChild) {
         
-        colorPalette.removeChild(colorPalette.firstChild)
+        sketch.removeChild(sketch.firstChild)
     }
 
     for (let i = 0; i < 1000; i++) {
         // create the element
+
         const square = document.createElement('div')
         // set the properties
+
         square.classList.add('square')
         square.style.backgroundColor = randomRGB()
-        square.addEventListener('hover', event => {
+        square.addEventListener('mouseenter', event => {
             console.log(event.target.style.backgroundColor)
             const color = event.target.style.backgroundColor
             // make a new square
+
             const newSquare = document.createElement('div')
             // set the props of the square
+
             newSquare.classList.add('square')
             newSquare.style.backgroundColor = color
             // append square to the DOM tree
+
             document.querySelector('#my-palette').appendChild(newSquare)
         })
         // append the element to the DOM tree
-        colorPalette.appendChild(square)
+
+        sketch.appendChild(square)
     }
 }
 
