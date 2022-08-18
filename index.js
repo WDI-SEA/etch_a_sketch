b1553d6dbc237de456b1865b570944f2e6256572
yasahaverland <yasahaverland@icloud.com>
2022-08-18 00:49:55 -0400

    Homework Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// The magic happens here!
+const pix = document.querySelector(".pixel")
+const screen = document.querySelector("#screen")
+
+
+const colorPix = (e) => {
+    e.target.classList.add("colored-in")
+}
+
+const makePix = () => {
+    for(let i = 0; i < 8003; i++){
+        const divPix = document.createElement('div')
+        divPix.classList.add('pixel')
+        divPix.addEventListener("mouseenter", colorPix)
+        screen.append(divPix)
+    }
+}
+makePix()
+
+
+
