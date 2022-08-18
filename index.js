96295d15e241ce5a147f067ea0629a45e8d9ef7a
brandon-w1205 <bwu1205@gmail.com>
2022-08-17 18:32:29 -0700

    adds addEventListener, etchBox to select the screen, for loop to create pixels on screen, querySelectorAll to grab the pixels, function etchasketch to create the addEventListeners on mouseenter, currently trying to implement a click feature to stop the etch a sketch

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // The magic happens here!
+document.addEventListener('DOMContentLoaded', () => {
+    // 
+
+    let etchBox = document.querySelector('#screen');
+    
+    for(let i = 0; i < 6824; i++) {
+        var makePixels = document.createElement('div');
+        makePixels.setAttribute("class", "pixel")
+        etchBox.appendChild(makePixels);
+    }
+    let p = document.querySelectorAll('.pixel');
+
+    let onOff = 0;
+    function etchasketch() {
+        onOff++
+        console.log(onOff)
+        if(onOff % 2 != 0){
+            p.forEach(e => {
+                e.addEventListener('mouseenter', () => {
+                    e.style.backgroundColor = 'rgb(0, 0, 0)';
+                })
+            })
+        } else {
+            p.forEach(e => {
+                e.removeEventListener('mouseenter', () => {
+                    e.style.backgroundColor = 'rgb(0, 0, 0)';
+                })
+            })
+        }
+        
+    }
+
+    
+
+    etchBox.addEventListener('click', etchasketch)
+    
+    
+    
+        
+
+    
+    
+})
