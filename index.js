3cae882a36ea11bb7ed016d794c74561167ff0ef
devin-lynch <devin.l.lynch@gmail.com>
2022-08-17 21:53:28 -0500

    Make etchasketch fully functional with reset button

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-// The magic happens here!
-const pixel = document.querySelector('.pixel')
-// console.log(pixel)
+document.addEventListener('DOMContentLoaded', () => {
+    
+
+    let pixels;
 const screen = document.querySelector('#screen')
 const clear = document.querySelector('#clear-screen-button')
-console.log(clear)
+
 
 // to fill the board with all the pixels
 const fillBoard = () => {
     for (let i = 0; i < 6199; i++){
         // create the element
-        const pixel = document.createElement('div')
+       const addPixel = document.createElement('div')
         // set the properties
-        pixel.classList.add('pixel')
-
-
-        screen.appendChild(pixel)
+        addPixel.classList.add('pixel')
+        // append the element
+        screen.appendChild(addPixel)
     }
+    pixels = document.querySelectorAll('.pixel')
 }
 
-pixel.addEventListener('mouseenter', () => {
-    pixel.classList.add('colored-in')
+fillBoard()
+
+function colorPixel(){
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseenter', () => {
+            pixel.classList.add('colored-in')
+        })
+    })
+}
+
+function clearPixel(){
+    pixels.forEach(pixel => {
+        clear.addEventListener('click', () => {
+            pixel.classList.remove('colored-in')
+        })
+    })
+}
+
+colorPixel()
+clearPixel()
+
 })
 
 
 
 
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    fillBoard()
-})
 
