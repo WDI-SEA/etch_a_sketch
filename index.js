17213443fa47f7e95b5ca44091a12da113fe4d7c
tatebonham <tatebonham@gmail.com>
2022-08-17 18:53:28 -0700

    Added forLoops to create squares and to give them the mouseover eventlistener, set up clear screen button

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // The magic happens here!
+document.addEventListener('DOMContentLoaded', () => {
+console.log('hi')
+
+    for(let i = 0; i < 4691; i++){
+        const newPixel = document.createElement('div')
+        newPixel.classList.add('pixel')
+
+        document.querySelector('#screen').appendChild(newPixel)
+    }
+
+
+    const pixelClass = document.querySelectorAll('.pixel')
+    // pixelClass.forEach((square, index) => {
+    //     square.addEventListener("mouseover", () => {
+    //         square.classList.add('colored-in')
+    //     });
+    // })
+    for(let i = 0; i < pixelClass.length; i++){
+        let pixel = pixelClass[i]
+        pixel.addEventListener('mouseover', () => {
+            pixel.classList.add('colored-in')
+        });
+    }
+
+    const clearBtn = document.getElementById('clear-screen-button')
+    
+    clearBtn.addEventListener('click', () => {
+        pixelClass.forEach(square => {
+            square.classList.remove('colored-in')
+        })
+    })
+
+    
+
+   
+
+})
