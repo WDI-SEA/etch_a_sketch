35513d79093a7e665047533b9d9db78a4ecc01ea
Shazammon <ammonorgill@gmail.com>
2022-08-17 19:32:02 -0600

    finished and made the pixels half the size

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // The magic happens here!
 document.addEventListener('DOMContentLoaded', () => {
     let screen = document.querySelector('#screen')
-    let pixels = document.querySelectorAll(".pixel")
+    let pixels ;
     let clear = document.querySelector("#clear-screen-button")
-    console.log(pixel)
     console.log(clear)
 
-    for (i = 0; i < 5000; i++) {
+    for (i = 0; i < 13000; i++) {
         //create element
         const newPixel = document.createElement('div')
         // give element properties
         newPixel.classList.add('pixel')
-        newPixel.addEventListener('mouseenter', () => {
-            newPixel.classList.add('colored-in');    
-        })
+        // newPixel.addEventListener('mouseenter', () => {
+        //     newPixel.classList.add('colored-in');    
+        // })
         //assign to DOM tree
         screen.append(newPixel)
     }
+    pixels = document.querySelectorAll(".pixel")
 
     // function color (e) {
     //     e.target.classList.add('colored-in')
@@ -25,15 +25,22 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // pixel.addEventListener('mouseenter', color);
 
-
-    pixel.addEventListener('mouseenter', () => {
-        pixel.classList.add('colored-in')
-        console.log(pixel.className)
+    pixels.forEach(taco => {
+        taco.addEventListener('mouseenter', () => {
+            taco.classList.add('colored-in')
+            
+        })
     })
 
     clear.addEventListener('click', () => {
-        let pixels = document.querySelectorAll(".colored-in")
-        pixels.classList.remove('colored-in')
+        pixels.forEach(pixel => {
+            pixel.classList.remove('colored-in')
+        })
+
+        
+        
+        // let pixels = document.querySelectorAll(".colored-in")
+        // pixels.classList.remove('colored-in')
 
     })
 
