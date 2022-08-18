91d1846f8c121dab7c62b0372c1b155ab3caaa11
allent13 <allentang113@gmail.com>
2022-08-17 18:50:57 -0700

    finishes etch-a-sketch

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // The magic happens here!
+
+const pixel = document.querySelector('.pixel')
+const clear = document.querySelector('#clear-screen-button')
+
+pixel.addEventListener('mouseenter', (e) => {
+    e.target.classList.add('colored-in')
+})
+
+const createPixels = (number) => {
+    const screen = document.querySelector('#screen')
+    for (let i = 0; i < number; i++) {
+        const newPixel = document.createElement('div')
+        newPixel.classList.add('pixel')
+        newPixel.addEventListener('mouseenter', (e) => {
+            e.target.classList.add('colored-in')
+        })
+        screen.append(newPixel)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    createPixels(5000)
+    clear.addEventListener('click', () => {
+        const pixels = document.querySelectorAll('.pixel')
+        for (let i = 0; i < pixels.length; i++) {
+            pixels[i].classList.remove('colored-in')
+        }
+    })
+})
