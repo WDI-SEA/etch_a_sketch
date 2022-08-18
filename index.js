4eee80f298027e953a805b0a32205d09d14e01dc
tpalmer111D <timpalmer@Tims-MacBook-Pro.local>
2022-08-18 09:45:34 -0600

    Second attempt to push, same js file added

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // The magic happens here!
+addEventListener("DOMContentLoaded", () => {
+    const screen = document.querySelector('#screen')
+    const button = document.querySelector('#clear-screen-button')
+
+
+
+    for (let i = 0; i < 3000; i++) {
+        const pixelDiv = document.createElement('div')
+        pixelDiv.classList.add('pixel')
+        screen.append(pixelDiv)
+    }
+        const pixels = document.querySelectorAll(".pixel")
+        pixels.forEach(pixel => {
+        pixel.addEventListener("mouseenter", () => {
+        pixel.style.backgroundColor = "black"
+    })
+})
+        button.addEventListener("click", () => {
+        pixels.forEach(pixel => {
+        pixel.style.backgroundColor = "white"
+    })
+})
+})
+
