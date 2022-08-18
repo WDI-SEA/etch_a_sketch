3b87c6cb83541ee78b52936e44119a75a734ab3b
JasonXChen <jasonxchen888@gmail.com>
2022-08-17 20:32:08 -0400

    Modify index.js to implement game board loading and functions

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// The magic happens here!
+const screen = document.querySelector("#screen");
+const clearBtn = document.querySelector("#clear-screen-button");
+
+const fillBoard = () =>
+{
+    for (let i = 0; i < 7937; i++)
+    {
+        const newPixel = document.createElement("div");
+        newPixel.classList.add("pixel");
+        screen.append(newPixel);
+        newPixel.addEventListener("mouseenter", () =>
+        {
+            newPixel.classList.add("colored-in")
+        })
+    }
+}
+clearBtn.addEventListener("click", () =>
+{
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel =>
+    {
+        pixel.classList.remove("colored-in");
+    })
+})
+
+document.addEventListener("DOMContentLoaded", () =>
+{
+    fillBoard();
+})
