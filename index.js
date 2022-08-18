f78a17897ac138aa9bf80f5fb19a66efdbfc3867
devin-lynch <devin.l.lynch@gmail.com>
2022-08-17 20:40:02 -0500

    Fill board pixels and set dom variables

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // The magic happens here!
+const pixel = document.querySelector('.pixel')
+// console.log(pixel)
+const screen = document.querySelector('#screen')
+const clear = document.querySelector('#clear-screen-button')
+console.log(clear)
+
+// to fill the board with all the pixels
+const fillBoard = () => {
+    for (let i = 0; i < 6199; i++){
+        // create the element
+        const pixel = document.createElement('div')
+        // set the properties
+        pixel.classList.add('pixel')
+
+
+        screen.appendChild(pixel)
+    }
+}
+
+pixel.addEventListener('mouseenter', () => {
+    pixel.classList.add('colored-in')
+})
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fillBoard()
+})
+
