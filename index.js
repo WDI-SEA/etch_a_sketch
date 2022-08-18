0cab218fd6e16032b640df79da756cd3de5e5e75
OmgValicious <ziallshamrock@gmail.com>
2022-08-17 21:03:46 -0700

    js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // The magic happens here!
+const screen = document.querySelector('#screen')
+const makePixel = () => {
+
+    for (let i = 0; i < 4001; i++) {
+      const pix = document.createElement('div')
+        pix.classList.add('pixel')
+        screen.appendChild(pix)
+    }
+        
+    let pixel = document.querySelector('.pixel')
+    pixel.addEventListener("mouseenter", function () {
+            pixel.classList.add("colored-in")
+        }) 
+    
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+     makePixel()
+    
+})
+    
+
+    
