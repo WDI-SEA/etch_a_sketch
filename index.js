532aa0e47ca36e976b19be772f6eaffdc8fc961e
joshdewhurst <joshuardewhurst@gmail.com>
2022-08-18 11:37:50 -0700

    Homework JD 8/18

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // The magic happens here!
+
+document.querySelector(".pixel").addEventListener("mouseenter", () => {
+     document.querySelector(".pixel").classList.add("colored-in")
+    })
+
+// for (i =0; i < 500; i++)
+
+const makePixel = () => {
+
+    for (i = 0; i < 5000; i++) {
+        const div = document.createElement("div")
+        div.classList.add("pixel")
+        document.getElementById("screen").appendChild(div)
+    } }
+
+// const sketchPad = () => {
+//     const pixel = document.querySelectorAll(".pixel")
+//     if (document.classList == "pixel") {
+//         pixel.addEventListener("mouseenter", () => {
+//         document.querySelector(".pixel").classList.add("colored-in")
+//         })
+//     }
+// }
+
+document.addEventListener('DOMContentLoaded', () => {
+    makePixel()
+
+    // let pix = document.querySelectorAll(".pixels")
+    // pix.forEach(() => {
+    //     pix.addEventListener("mouseenter", () => {
+    //         document.querySelector(".pixel").classList.add("colored-in")
+    //     })
+   
+    // const pix = document.querySelector(".pixel")
+    // const changeColor = () => {
+    //     pix.classList.add("colored-in")
+    // }
+
+    // pix.addEventListener("mouseenter", changeColor())
+
+    const sketch = () => {
+        document.querySelector(".pixel").classList.add("colored-in")
+    }
+    const pix = document.querySelectorAll("pixel")
+    pix.addEventListener("mouseenter", sketch(pix))
+
+})
