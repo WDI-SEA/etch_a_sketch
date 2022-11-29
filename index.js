5070a4189de8a4a0890e92c18da304b29f2bfe27
dudahamilton <mariaehamilton28@gmail.com>
2022-11-29 09:37:18 -0800

    completed deliverable

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // The magic happens here!
+
+let screen = document.querySelector('#screen')
+let pixel = document.querySelector('.pixel')
+
+// buttom
+let clearButton = document.querySelector('#clear-screen-button')
+
+pixel.addEventListener('mouseenter', () => {
+    pixel.classList.add('colored-in')
+})
+
+for (let i = 0; i < 3134; i++) {
+    let newPixel = document.createElement('div')
+    newPixel.classList.add('pixel')
+    // add mouseEnter function
+    newPixel.addEventListener('mouseenter', (e) => {
+        e.target.classList.add('colored-in')
+    })
+
+    screen.append(newPixel)
+}
+
+// Function to clear sccreen 
+
+let clearFunction = function () {
+    let everyPixel = document.querySelectorAll('.pixel')
+    everyPixel.forEach(pixel => {
+        pixel.classList.remove('colored-in')
+    })
+}
+
+clearButton.addEventListener('click', clearFunction)
