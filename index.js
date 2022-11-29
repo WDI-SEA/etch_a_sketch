9ab48177604272c4e4ccf44751de8e14535296c0
emilyjryan <emilyryanmac@gmail.com>
2022-11-28 20:38:53 -0800

    finished deliverable

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// The magic happens here!
+console.log('it works!')
+
+let firstSquare = document.querySelector('.pixel')
+let screen = document.querySelector('#screen')
+let start = document.querySelector('#start')
+let clear = document.querySelector('#clear-screen-button')
+
+firstSquare.addEventListener('mouseenter', (e) => {
+    firstSquare.classList.add('colored-in')
+})
+
+start.addEventListener('click', makePixels)
+    
+    function makePixels () {
+        for (let i=0; i<20000; i++) {
+            const square = document.createElement('div')
+            square.classList.add('pixel')
+            screen.appendChild(square)
+            
+            square.addEventListener('mouseenter', (e) => {
+                square.classList.add('colored-in')
+                
+            });
+        }
+        clear.addEventListener('click', (e) => {
+            const cubes = document.querySelectorAll('.colored-in');
+            console.log(cubes)
+            cubes.forEach(cube => {
+                cube.classList.remove('colored-in')})
+            })
+    } 
+
