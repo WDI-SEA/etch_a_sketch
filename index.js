dcca56bc6072c0861392513dbcb3350311fc6265
fmher <feumatthewher2@gmail.com>
2022-11-28 23:43:14 -0800

    fin

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// The magic happens here!
+const pixel = document.querySelector('.pixel')
+
+// console.log(pixel)
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // pixel.addEventListener('mouseenter', () => {
+    //     pixel.classList.add('colored-in')
+    //     // pixel.style.backgroundColor = 'black'  ---- Also works
+    // })
+    
+    for (let i = 0; i < 6929; i ++) {
+        const morePixels = document.createElement('div')
+        morePixels.classList.add('pixel')
+        document.querySelector('#screen').appendChild(morePixels)
+        
+    }
+
+    const pixels = document.querySelectorAll('.pixel')
+
+    
+
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseenter', () => {
+         pixel.classList.add('colored-in')
+       })
+     })
+
+})
+
+
+
+  document.querySelector('#clear-screen-button').addEventListener('click', () => {
+    const pixels = document.querySelectorAll('.pixel')
+    pixels.forEach(pixel => pixel.classList.remove('colored-in'))
+  })
+
+  
