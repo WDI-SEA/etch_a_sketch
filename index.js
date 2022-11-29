b5ca419569d2fa449233ea60b41cd784af89fec0
jaywomack <jaredwomack@Jareds-MBP.lan>
2022-11-28 17:31:54 -0800

    added DOM selectors

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // The magic happens here!
+const container = document.querySelector(".container")
+const screen = document.getElementById("screen")
+const pixel = document.querySelector(".pixel")
+const clearScreenButton = document.getElementById("clear-screen-button")
+const pixels = document.querySelectorAll(".pixel")
