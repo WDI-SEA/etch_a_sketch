9752801f8d253be60b8ec6241eda4143ea41458c
nancyd333 <rgba0001@gmail.com>
2022-11-28 22:12:42 -0500

    deliverable

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// The magic happens here!
+const pixel = document.querySelector('.pixel')
+
+const screen = document.querySelector('#screen')
+const button = document.querySelector('#clear-screen-button')
+
+const colorPixel = (event) => {
+    event.target.classList.add('colored-in')
+}
+
+function createPixels() {
+    for(i=0; i < 5723; i++){
+        const newPixel = document.createElement('div');
+        newPixel.classList.add('pixel')
+        screen.append(newPixel)
+        newPixel.addEventListener('mouseenter', colorPixel)
+    }
+}
+
+const clearScreen = () => {
+   const allColoredPixels = document.querySelectorAll('.colored-in')
+   allColoredPixels.forEach(item => {
+        item.remove('.colored-in')
+    })
+
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{ 
+    createPixels()
+    button.addEventListener('mousedown', clearScreen)
+ })
+
+
+
+
+
+
