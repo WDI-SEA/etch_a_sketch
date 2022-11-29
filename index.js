6a7c9319d93082ec07d06e5c6e79211d775baebf
tylersb <tylersb@gmail.com>
2022-11-28 17:35:49 -0800

    adds deliverable

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// The magic happens here!
+const pixel = document.querySelector('.pixel')
+const screen = document.querySelector('#screen')
+const clear = document.querySelector('#clear-screen-button')
+
+pixel.addEventListener('mouseenter', (e) => {
+  e.target.classList.add('colored-in')
+})
+
+for (let i = 0; i < 8000; i++) {
+  const newPixel = document.createElement('div')
+  newPixel.classList.add('pixel')
+  newPixel.addEventListener('mouseenter', (e) => {
+    e.target.classList.add('colored-in')
+  })
+  screen.appendChild(newPixel)
+}
+
+clear.addEventListener('click', () => {
+  const allPixels = document.querySelectorAll('.pixel')
+  allPixels.forEach(pix => {
+    pix.classList.remove('colored-in')
+  })
+})
