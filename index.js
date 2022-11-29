1001a5069f143419b2194d19a5728b87c891d607
paulyjkim1 <paulyjkim1@gmail.com>
2022-11-29 09:05:17 -0800

    complete deliverable

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// The magic happens here!
+let screen = document.getElementById('screen')
+let button = document.getElementById('clear-screen-button')
+
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i =1; i < 4000; i++){
+        const square = document.createElement('div')
+        square.classList.add('pixel')
+        screen.appendChild(square)
+        square.addEventListener('mouseenter', () => {
+            square.classList.add('colored-in')
+        })
+    }
+
+    button.addEventListener('click',()=>{
+        let divs = document.querySelectorAll('div')
+        divs.forEach(div => {
+            div.classList.remove('colored-in')
+        })
+    })
+})
