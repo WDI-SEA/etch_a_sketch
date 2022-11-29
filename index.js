2664c435af65fc4f45b87fe4a39df6ea86b95544
cailinshaffer <cailinshaffer@gmail.com>
2022-11-29 10:37:37 -0700

    add etch a sketch

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// The magic happens here!
+let screen = document.querySelector('#screen')
+let pixel = document.querySelector('.pixel')
+let clearButton = document.querySelector('#clear-screen-button')
+
+pixel.addEventListener('mouseenter', () => {
+    pixel.classList.add('colored-in')
+})
+
+for (let i = 0; i < 4600; i++){
+    let newPixel = document.createElement('div')
+    newPixel.classList.add('pixel')
+    newPixel.addEventListener('mouseenter', (e) => {
+        e.target.classList.add('colored-in')
+  
+    })
+    
+    screen.append(newPixel)
+}
+
+let clearFunction = function(){
+    let everyPixel = document.querySelectorAll('.pixel')
+    everyPixel.forEach(pixel => { 
+        pixel.classList.remove('colored-in')
+    })
+}
+clearButton.addEventListener('click', clearFunction)
+
+
+
+
+
+
