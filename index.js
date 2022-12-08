c1bfe6f77183ed8b8418cfc6659db0181279356c
alidix <aliyahdixon4@gmail.com>
2022-12-07 20:15:57 -0500

    deliverable completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // The magic happens here!
+
+let screen = document.querySelector("#screen");
+let pixel = document.querySelector(".pixel");
+
+// clear button
+let clearButton = document.querySelector("#clear-screen-button");
+
+pixel.addEventListener("mouseenter", () => {
+  pixel.classList.add("colored-in");
+});
+// loop nthrough and make newpixels
+for (let i = 0; i < 4735; i++) {
+  let newPixel = document.createElement("div");
+  newPixel.classList.add("pixel");
+
+  //  add mouseEnter functionality
+  newPixel.addEventListener("mouseenter", (e) => {
+    e.target.classList.add("colored-in");
+  });
+  screen.append(newPixel);
+}
+
+let clearFunction = function () {
+  let everyPixel = document.querySelectorAll(".pixel");
+  everyPixel.forEach((pixel) => {
+    pixel.classList.remove("colored-in");
+  });
+};
+clearButton.addEventListener("click", clearFunction);
